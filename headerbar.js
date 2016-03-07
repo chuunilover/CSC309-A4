2df13d9901e5273690672757d673a40a06c69370
Alex Tse <chuunilover@gmail.com>
2016-03-07 16:39:57 -0500

    Added help page and links between help, search, login pages.

--- headerbar.js
+++ headerbar.js
@@ -1,13 +1,16 @@
 window.onload = function() {
 	$("#searchbox").width($(window).width() - 250);
+	$("#header").width($(window).width() - 10);
 }
 
 function process_search(event){
 	if(event.keyCode == 13){
 		alert("Searching for restaurants with \"" + $("#searchbox").val() + "\"");
+		window.open("searchresults.html", "_self")
 	}
 }
 
 function resizeSearchBar(){
 	$("#searchbox").width($(window).width() - 250);
+	$("#header").width($(window).width() - 10);
 }
