9f6f247f060cf817988f3d15611f26195c9b3432
chrisod30 <chrisod30@hotmail.com>
2016-04-02 19:02:14 -0400

    Added files via upload
    Added the reservation pages. Tables are not clickable yet but I'm working on it.

---
+++ reservation.js
@@ -0,0 +1,406 @@
+var c = document.getElementById("myCanvas");
+
+var table1 = c.getContext("2d");
+table1.beginPath();
+table1.arc(95,60,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,24,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,24,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,96,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,96,10,0,2*Math.PI);
+chair.stroke();
+
+//----------------------------------------------
+
+var table2 = c.getContext("2d");
+table1.beginPath();
+table1.arc(250,60,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,24,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,24,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,96,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,96,10,0,2*Math.PI);
+chair.stroke();
+
+//-------------------------------------------------
+
+var table3 = c.getContext("2d");
+table1.beginPath();
+table1.arc(405,60,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(360,24,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(450,24,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(360,96,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(450,96,10,0,2*Math.PI);
+chair.stroke();
+
+//-----------------------------------------------------
+
+var table4 = c.getContext("2d");
+table1.beginPath();
+table1.arc(95,190,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,154,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,154,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,226,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,226,10,0,2*Math.PI);
+chair.stroke();
+
+//---------------------------------------------------
+
+var table5 = c.getContext("2d");
+table1.beginPath();
+table1.arc(250,190,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,154,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,154,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,226,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,226,10,0,2*Math.PI);
+chair.stroke();
+
+
+//---------------------------------------------------------------
+
+var table6 = c.getContext("2d");
+table1.beginPath();
+table1.arc(95,330,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,294,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,366,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,366,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,294,10,0,2*Math.PI);
+chair.stroke();
+
+//---------------------------------------------------
+
+var table7 = c.getContext("2d");
+table1.beginPath();
+table1.arc(250,330,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,294,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,294,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,366,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,366,10,0,2*Math.PI);
+chair.stroke();
+
+//---------------------------------------------------------
+
+var table8 = c.getContext("2d");
+table1.beginPath();
+table1.arc(405,330,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(360,294,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(450,294,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(360,366,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(450,366,10,0,2*Math.PI);
+chair.stroke();
+
+//---------------------------------------------------------
+
+var table9 = c.getContext("2d");
+table1.beginPath();
+table1.arc(95,470,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,434,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,434,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,506,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,506,10,0,2*Math.PI);
+chair.stroke();
+
+//---------------------------------------------------
+
+var table10 = c.getContext("2d");
+table1.beginPath();
+table1.arc(250,470,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,434,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,434,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,506,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,506,10,0,2*Math.PI);
+chair.stroke();
+
+//---------------------------------------------------------
+
+var table11 = c.getContext("2d");
+table1.beginPath();
+table1.arc(405,470,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(360,434,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(450,434,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(360,506,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(450,506,10,0,2*Math.PI);
+chair.stroke();
+
+//----------------------------------------------------------------
+
+var table12 = c.getContext("2d");
+table1.beginPath();
+table1.arc(95,610,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,574,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,574,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(50,646,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(140,646,10,0,2*Math.PI);
+chair.stroke();
+
+//---------------------------------------------------
+
+var table13 = c.getContext("2d");
+table1.beginPath();
+table1.arc(250,610,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,574,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,574,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(205,646,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(295,646,10,0,2*Math.PI);
+chair.stroke();
+
+//---------------------------------------------------------
+
+var table14 = c.getContext("2d");
+table1.beginPath();
+table1.arc(405,610,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(360,574,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(450,574,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(360,646,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(450,646,10,0,2*Math.PI);
+chair.stroke();
+
+
+//---------------------------------------------------------
+
+var table15 = c.getContext("2d");
+table1.beginPath();
+table1.arc(560,610,40,0,2*Math.PI);
+table1.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(515,574,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(605,574,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(515,646,10,0,2*Math.PI);
+chair.stroke();
+
+var chair = c.getContext("2d");
+chair.beginPath();
+chair.arc(605,646,10,0,2*Math.PI);
+chair.stroke();
